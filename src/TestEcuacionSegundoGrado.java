014fdeb2f7ed47eb638951e3d52e397b5a928c34
Alejandro A. <alex@gmail.com>
2018-12-14 12:17:55 +0100

    EcuacionSegundoGrado Terminada

--- src/TestEcuacionSegundoGrado.java
+++ src/TestEcuacionSegundoGrado.java
@@ -6,15 +6,31 @@ public class TestEcuacionSegundoGrado {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Introduce el valor a: ");
+		System.out.println("Solucionador de ecuaciones de 2º grado.");
+		System.out.println("Forma de la ecuación: ax[2] + bx + c = 0");
+		System.out.println("");
+		
+		System.out.print("Introduce el valor a: ");
 		float a = sc.nextFloat();
-		System.out.println("Introduce el valor b: ");
+		System.out.print("Introduce el valor b: ");
 		float b = sc.nextFloat();
-		System.out.println("Introduce el valor c: ");
+		System.out.print("Introduce el valor c: ");
 		float c = sc.nextFloat();
 		
+		sc.close();
+		
 		EcuacionSegundoGrado e = new EcuacionSegundoGrado( a, b, c);
-
+		
+		if (! e.esResoluble()) {
+			System.out.println("La ecuación no es resoluble.");
+			System.exit(1);
+		}
+		
+		if (e.calcularX1() == e.calcularX2())
+			System.out.println("La solución es x = " + e.calcularX1());
+		else
+			System.out.println("La solución de la ecuación es x1 = " + e.calcularX1() + " y x2 = " + e.calcularX2());
+		
 	}
 
 }
